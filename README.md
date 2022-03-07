c8211f0d220dabf195968ca3c2ce4747c724450d
Minjung Sung <minjungsung1994@gmail.com>
2022-03-08 00:24:35 +0900

    Rabin-Karp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # algorithm
+
+## 라빈-카프 알고리즘 (Rabin-Karp)
---
+++ src/Rabin-Karp.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+// ws: word string, ps: pattern string
+void findString(string ws, string ps) {
+  int wsSize = ws.size();
+  int psSize = ps.size();
+
+  int wsHash = 0;
+  int psHash = 0;
+  int power = 1; 
+
+  for(int i = 0; i <= wsSize - psSize; i++) {
+    if (i == 0) {
+      for (int j = 0; j < psSize; j++) {
+        wsHash += ws[psSize - 1 - j] * power;
+        psHash += ps[psSize - 1 - j] * power;
+        if (j < psSize - 1) power *= 3;
+      }
+    } else {
+      wsHash = 3 * (wsHash - ws[i - 1] * power) + ws[psSize - 1 + i];
+    }
+    if (wsHash == psHash) {
+      bool isFind = true;
+      // 우연히 해시값이 겹친 경우
+      for (int j = 0; i < psSize; j++) {
+        if (ws[i + j] != ps[j]) {
+          isFind = false;
+          break;
+        }
+      }
+      if (isFind) {
+        cout << wsHash << " " << psHash << endl;
+        printf("% 번째서 발견\n", i + 1);
+      }
+    }
+  }
+}
+int main() {
+  string ws = "AABDCDABD";
+  string ps = "ABD";
+  findString(ws, ps);
+}
